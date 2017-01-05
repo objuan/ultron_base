4c4fe9ea651eb31fc221822a90c5caa7ead0ce4d
objuan <cecchinimarco71@gmail.com>
2017-01-05 07:33:23 +0100

    aa

---
+++ src/ultron_description/env-hooks/50.ultron_description.sh
@@ -0,0 +1,1 @@
+export ULTRON_DESCRIPTION=$(catkin_find --without-underlays --first-only ultron_description urdf/description.xacro 2>/dev/null)
---
+++ src/ultron_description/view_ultron.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "View RVIZ Test .. V1.0 "
+
+roslaunch ultron_description display.launch model:='$(find ultron_description)/urdf/description.xacro'
